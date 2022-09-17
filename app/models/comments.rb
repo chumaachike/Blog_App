90d071ab446a5e7527f6f04988457bfe05350583
Edward Achike <edwardachike@gmail.com>
2022-09-17 22:52:33 +0100

    add post counter

--- app/models/comments.rb
+++ app/models/comments.rb
@@ -1,7 +1,7 @@
 class Comments < ApplicationRecord
   belongs_to :author, class_name: 'Users'
   belongs_to :posts, class_name: 'Posts'
-  
+
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
