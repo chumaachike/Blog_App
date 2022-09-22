c2c5b1f258eb7e7f4ff84245f6fbabdc78f4aef1
Edward Achike <edwardachike@gmail.com>
2022-09-22 12:22:04 +0100

    test files for models and model validation for user and post done

--- db/migrate/20220917161645_rename_posts_again.rb
+++
@@ -1,6 +0,0 @@
-class RenamePostsAgain < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :comments, :post_id, :posts_id
-    rename_column :likes, :post_id, :posts_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_161645) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_094143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,25 +19,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_161645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
-    t.bigint "posts_id", null: false
+    t.bigint "post_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
-    t.bigint "posts_id", null: false
+    t.bigint "post_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
-    t.index ["posts_id"], name: "index_likes_on_posts_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.integer "comments_counter"
-    t.integer "likesCounter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
@@ -46,16 +46,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_161645) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
     t.string "bio"
-    t.integer "posts_counter"
+    t.string "photo"
+    t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "posts", column: "posts_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
