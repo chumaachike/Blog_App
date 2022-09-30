8edc0dd4ec4a97f65be6c269a71b934c22e5d5a4
Edward Achike <edwardachike@gmail.com>
2022-09-30 07:41:59 +0100

    add create capacity

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,38 @@
 class PostsController < ApplicationController
   def index
+    @current_user = current_user
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
+    @current_user = current_user
     @post = Post.find(params[:id])
     @comments = @post.comments
+  end
+  def new
+    @current_user = current_user
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: {post: @post}}
+    end
+  end
+  def create
+    @current_user = current_user
+    post = Post.new
+    post.title = params[:user_posts][:title]
+    post.text = params[:user_posts][:text]
+    post.author = current_user
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = 'Post was successfully created'
+          redirect_to user_path(current_user)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new, new_user_post_path(current_user)
+        end
+      end
+    end
   end
 end
