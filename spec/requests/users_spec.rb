381ff8ccd6a4dac6a11d4fabfa95575eeff3451e
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-04 22:10:55 +0200

    Add test for user routes

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,63 +1,26 @@
-# require 'rails_helper'
-
-# RSpec.describe 'Users', type: :request do
-#   describe 'GET /' do
-#     before(:each) do
-#       get '/'
-#     end
-#     it 'returns http success' do
-#       expect(response).to have_http_status(:success)
-#     end
-#     it 'render template users/index' do
-#       expect(response).to render_template('users/index')
-#     end
-#   end
-
-#   describe 'GET /users' do
-#     before(:each) do
-#       get '/users'
-#     end
-#     it 'returns http success' do
-#       expect(response).to have_http_status(:success)
-#     end
-#     it 'render template users/index' do
-#       expect(response).to render_template('users/index')
-#     end
-#   end
-
-#   describe 'GET /users/1' do
-#     before(:each) do
-#       get '/users/1'
-#     end
-#     it 'returns http success' do
-#       expect(response).to have_http_status(:success)
-#     end
-#     it 'render template users/show' do
-#       expect(response).to render_template('users/show')
-#     end
-#   end
-
-#   describe 'GET /users/index' do
-#     before(:each) do
-#       get '/users/index'
-#     end
-#     it 'returns http success' do
-#       expect(response).to have_http_status(:success)
-#     end
-#     it 'not render template users/index' do
-#       expect(response).to_not render_template('users/index')
-#     end
-#   end
-
-#   describe 'GET /users/show' do
-#     before(:each) do
-#       get '/users/show'
-#     end
-#     it 'returns http success' do
-#       expect(response).to have_http_status(:success)
-#     end
-#     it 'render template users/show' do
-#       expect(response).to render_template('users/show')
-#     end
-#   end
-# end
+require 'rails_helper' 
+RSpec.describe 'Users', type: :request do 
+    describe 'GET /' do 
+        before(:each) do 
+            @lilly = User.create(name: 'Lilly', photo: 'https://c.tenor.com/YIeHLcvImMsAAAAM/meditation-dog.gif', bio: 'Teacher from Poland') 
+            Post.create(author: @lilly, title: 'Hello', text: 'This is my first post') 
+            Post.create(author: @lilly, title: 'Hey', text: 'This is my second post') 
+            get '/'
+         end 
+         it 'returns http success' do 
+            expect(response).to have_http_status(:success) 
+        end 
+        it 'render template users/index' do 
+            expect(response).to render_template('users/index') 
+        end 
+        it 'returns http success' do 
+            expect(response).to have_http_status(:success) 
+        end 
+        it 'render template users/index'
+         do expect(response).to render_template('users/index') 
+        end 
+        it 'returns http success' 
+        do expect(response).to have_http_status(:success) 
+        end 
+    end 
+end 
