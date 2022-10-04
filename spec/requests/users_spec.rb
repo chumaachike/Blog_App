ce0b30e943e4e0ee1e307f214727823bda66cb2e
Edward Achike <edwardachike@gmail.com>
2022-10-04 21:39:53 +0100

    fixing user spec linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 require 'rails_helper'
-
 RSpec.describe 'Users', type: :request do
   describe 'GET /' do
     before(:each) do
@@ -27,31 +25,3 @@ RSpec.describe 'Users', type: :request do
     end
   end
 end
-=======
-require 'rails_helper' 
-RSpec.describe 'Users', type: :request do 
-    describe 'GET /' do 
-        before(:each) do 
-            @lilly = User.create(name: 'Lilly', photo: 'https://c.tenor.com/YIeHLcvImMsAAAAM/meditation-dog.gif', bio: 'Teacher from Poland') 
-            Post.create(author: @lilly, title: 'Hello', text: 'This is my first post') 
-            Post.create(author: @lilly, title: 'Hey', text: 'This is my second post') 
-            get '/'
-         end 
-         it 'returns http success' do 
-            expect(response).to have_http_status(:success) 
-        end 
-        it 'render template users/index' do 
-            expect(response).to render_template('users/index') 
-        end 
-        it 'returns http success' do 
-            expect(response).to have_http_status(:success) 
-        end 
-        it 'render template users/index'
-         do expect(response).to render_template('users/index') 
-        end 
-        it 'returns http success' 
-        do expect(response).to have_http_status(:success) 
-        end 
-    end 
-end 
->>>>>>> a3cfed9612afbb7c52ee5f35b22d4952ad9af400
